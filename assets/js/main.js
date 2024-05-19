2bfa437ecc7958526360dca4c6bef36e13fbedc7
Alexander Piniuhin <85253534+Piniuhin@users.noreply.github.com>
2024-05-19 18:42:00 +0200

    add dark

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,3 +139,52 @@ sr.reveal(`.home__image`, { origin: "bottom" });
 sr.reveal(`.about__data, .skills__data`, { origin: "left" });
 sr.reveal(`.home__image, .skills__content`, { origin: "right" });
 sr.reveal(`.projects__card`, { interval: 100 });
+
+/*==================== DARK LIGHT THEME ====================*/
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line"; //  uil-sun
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/*=============== reCAPTCHA ===============*/
+
+// function onClick(e) {
+//   e.preventDefault();
+//   grecaptcha.enterprise.ready(async () => {
+//     const token = await grecaptcha.enterprise.execute(
+//       "6LfJhtwpAAAAAKeB3C3AjHiTmdqM8Yt6A08A4d_b",
+//       { action: "LOGIN" }
+//     );
+//   });
+// }
